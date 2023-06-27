2fa1bf3f667c4442acd4fa6b9631c3b5b251c6eb
RileyManda <rileymanda69@gmail.com>
2023-06-27 20:06:39 +0200

    Implemented click styling to refresh and submit btn

--- src/getScore.js
+++ src/getScore.js
@@ -1,5 +1,5 @@
 export const getScores = () => fetch(
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/dumG1PB40BpIOBdmh4JI/scores',
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5DOHI6Jz0kZNClOyq4yg/scores',
 )
   .then((response) => response.json())
   .then((data) => data.result);
