37d11e96f26d31b367fc782e48dc6b5b0e31f582
RileyManda <rileymanda69@gmail.com>
2023-06-27 15:32:18 +0200

    Implemented addScore function to add score to  api

---
+++ src/addScore.js
@@ -0,0 +1,39 @@
+export const submitScore = () => {
+  const scoreForm = document.getElementById('scoreForm');
+  scoreForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const userNameInput = document.getElementById('userNameInput');
+    const scoreInput = document.getElementById('scoreInput');
+
+    const user = userNameInput.value;
+    const score = parseInt(scoreInput.value, 10);
+
+    const data = {
+      user,
+      score,
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lWMo5174rgz1dWg1Q9yt/scores',
+      requestOptions,
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        // Handle the response here
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Handle error here
+      });
+  });
+};
+export default submitScore;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles.css';
 import populateScoreTable from './scoreTable.js';
+import { submitScore } from './addScore.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   populateScoreTable();
+  submitScore();
 });
